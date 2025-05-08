ae1495f1b9398639705f2195834a529f1f00c44c
anirudhsalgundi <anirudhsalgundi@gmail.com>
2025-05-08 16:21:30 +0530

    added more packages

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,5 @@
 # lets add whatever we will have to
 brew install chktex
 brew install chkbit
-sudo tlmgr install latexmk
-sudo tlmgr install chktex
-sudo tlmgr install texcount
+sudo tlmgr update --self
+sudo tlmgr install lastpage multirow adjustbox balance txfonts texcount chktex latexmk
